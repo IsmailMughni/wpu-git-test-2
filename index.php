af323e0451ee6a468b0babe11847d90c10b25554
ismail <ismailmughni@gmail.com>
2022-01-29 06:45:31 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <h1>Hello World! </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aliquam velit praesentium dolorum, quae doloremque in perferendis fugiat accusantium. Reiciendis quo maxime soluta. Aliquid, magni hic quos dolores in omnis.</p>
 </body>
 </html>
