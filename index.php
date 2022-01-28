a4125d611cbabe30ab9f81ca736ccc6cb7f17833
ismail <ismailmughni@gmail.com>
2022-01-29 06:53:16 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World! </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem aliquam velit praesentium dolorum, quae doloremque in perferendis fugiat accusantium. Reiciendis quo maxime soluta. Aliquid, magni hic quos dolores in omnis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
